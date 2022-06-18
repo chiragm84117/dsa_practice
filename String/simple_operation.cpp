b346b97bda643f2a430122d69092ef85a3c04915
chirag makwana <lavishm84117@gmail.com>
2022-06-18 16:11:29 +0530

    string length funtion

---
+++ String/simple_operation.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+void length(char *a){
+    int i;
+    for (i=0;a[i]!='\0';i++){
+
+    }
+    cout<<"length is "<<i<<endl;
+}
+
+int main(){
+    char *a="welcome";
+    length(a);
+}
