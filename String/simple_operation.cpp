1bc67b27e45cffe37a71702aa885439d696136c6
chirag makwana <lavishm84117@gmail.com>
2022-06-18 16:21:03 +0530

    function of upper case change

--- String/simple_operation.cpp
+++ String/simple_operation.cpp
@@ -1,14 +1,25 @@
 #include<iostream>
 using namespace std;
-void length(char *a){
+void length(char a[]){
     int i;
     for (i=0;a[i]!='\0';i++){
 
     }
     cout<<"length is "<<i<<endl;
+    
+}
+
+void u_case(char a[]){
+    // diff between upper and lower case in 32 ACIIE CODE
+    for(int i=0;a[i]!='\0';i++){
+        a[i]=a[i]-32;
+    }
+    cout<<a<<endl;
 }
 
 int main(){
-    char *a="welcome";
+    char a[]="welcome";
     length(a);
+    u_case(a);
+    
 }
