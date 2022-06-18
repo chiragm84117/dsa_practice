a52a83982718e28d9903f29e567734fed1b0959c
chirag makwana <lavishm84117@gmail.com>
2022-06-18 16:26:06 +0530

    toggle function added

--- String/simple_operation.cpp
+++ String/simple_operation.cpp
@@ -17,9 +17,22 @@ void u_case(char a[]){
     cout<<a<<endl;
 }
 
+void toggle(char a[]){
+    for(int i=0;a[i]!='\0';i++){
+        if(a[i]>=65 && a[i]<=90){
+            a[i]+=32;
+        }
+        // this values id accie code values for characters
+        else if(a[i]>=97 && a[i]<=122){
+            a[i]-=32;
+        }
+    }
+    cout<<a<<endl;
+}
+
 int main(){
     char a[]="welcome";
     length(a);
     u_case(a);
-    
+    toggle(a);
 }
