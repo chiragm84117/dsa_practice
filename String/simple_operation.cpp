dd80e6a5c25bd5074f84f7834d4c590eb864309a
chirag makwana <lavishm84117@gmail.com>
2022-06-18 16:45:35 +0530

    funtion for counting vowels

--- String/simple_operation.cpp
+++ String/simple_operation.cpp
@@ -30,9 +30,26 @@ void toggle(char a[]){
     cout<<a<<endl;
 }
 
+void vowels(char a[]){
+    int vcount=0,count=0;
+
+    for(int i=0;a[i]!='\0';i++){
+        if(a[i]=='a'||a[i]=='e'||a[i]=='i'||a[i]=='o'||a[i]=='u'||a[i]=='A'||a[i]=='E'||a[i]=='I'||a[i]=='O'||a[i]=='O'){
+            vcount++;
+        }
+        else if (a[i]>=65 && a[i]<=90 && a[i]>=97 && a[i]<=122){ //to avoid space and special charaters
+            count++;
+        }
+    }
+    cout<<"no is vowels are "<<vcount<<" and the no of consonents are "<<count<<endl;
+}
+
 int main(){
     char a[]="welcome";
     length(a);
     u_case(a);
     toggle(a);
+
+    char b[]="my name is chirag";
+    vowels(b);
 }
